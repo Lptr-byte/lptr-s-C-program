42208bd5bf0200797fc4199973c07fef257e6101
Lptr-byte <2019266396@qq.com>
2023-02-04 17:33:38 +0800

    P6183

---
+++ P6183.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, vis[32800], temp[20], ans[32800][20], goal[20];
+
+int Hash(){
+	int res = 0;
+	for(int i = 1; i <= n; i++)
+		if(temp[i])
+			res += (1 << (i - 1));
+	return res;
+}
+
+void print(){
+	for (int i = 0; i < (1 << n); i++) {
+		for (int j = 1; j <= n; j++) {
+			if (ans[i][j] == 0)
+				cout << 0;
+			else
+              cout << "X";
+            if (j == n)
+              cout << endl;
+          }
+    }
+	for(int i = 1; i <= n; i++)
+		cout << 0;
+	cout << endl;
+}
+
+void dfs(int step){
+	if(step == (1 << n)){
+		print();
+		exit(0);
+		return;
+	}
+	for(int i = 1; i <= n; i++){
+		temp[i] = !temp[i];
+		int h = Hash();
+		//cout << "step" << step << ":" << "hash = " << h <<endl;
+		//for(int i = 1; i <= 20; i++)
+		//	cout << temp[i];
+		//cout << endl;
+		if(!vis[h]){
+			vis[h] = 1;
+			memcpy(ans[step], temp, sizeof(temp));
+			dfs(step + 1);
+			vis[h] = 0;
+		}
+	}
+}
+
+void solve(){
+	cin >> n;
+	vis[0] = 1;
+	dfs(1);
+}
+
+int main(){
+	solve();
+	return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+using namespace std;
+int temp[20], n;
+
+int Hash(){
+	int res = 0;
+	for(int i = 1; i <= n; i++)
+		if(temp[i])
+			res += (1 << (i - 1));
+	return res;
+}
 
 int main(){
-	printf("test\n");
-
+	int a = (1 << 3);
+	printf("%d\n", a);
+	cin >> n;
+	for(int i = 1; i <= 15; i++){
+		temp[i] = !temp[i];
+		for(int i = 1; i <= 15; i++)
+			cout << temp[i];
+		cout << endl;
+		cout << Hash() << endl;
+		printf("%d\n", Hash());
+	}
 	return 0;
 }
